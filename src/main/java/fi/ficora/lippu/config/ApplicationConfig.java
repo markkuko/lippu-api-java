e3ca45b8cbe19c7ce180915ef823bc66ce1e725b
Markku Korkeala <markku.korkeala@solita.fi>
2017-09-30 21:47:22 +0300

    Add: inital support for localization

--- src/main/java/fi/ficora/lippu/config/ApplicationConfig.java
+++ src/main/java/fi/ficora/lippu/config/ApplicationConfig.java
@@ -1,7 +1,9 @@
 package fi.ficora.lippu.config;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.io.ClassPathResource;
 
 import org.springframework.core.io.Resource;
@@ -18,5 +20,12 @@ class ApplicationConfig {
         Jackson2RepositoryPopulatorFactoryBean factory = new Jackson2RepositoryPopulatorFactoryBean();
         factory.setResources(new Resource[] { sourceData });
         return factory;
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("locale/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
     }
 }
